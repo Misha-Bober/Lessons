ace8a3283f25ec31bf639036a82c3d64a0490b3a
Misha-Bober <144991058+Misha-Bober@users.noreply.github.com>
2023-12-04 15:50:20 +0200

    Add files via upload

--- lesson_18/js/script.js
+++ lesson_18/js/script.js
@@ -1,5 +1,5 @@
 
-const icon = document.querySelector('.icon-menu');
+const icon = document.querySelector('.icon-menu-burrer');
 icon.addEventListener('click', function () {
 	document.documentElement.classList.toggle('menu-open');
 });

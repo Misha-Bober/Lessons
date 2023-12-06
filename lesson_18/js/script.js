6b664fc992937ab59ebf62483726895e2c02dd67
Misha-Bober <144991058+Misha-Bober@users.noreply.github.com>
2023-12-06 09:01:05 +0200

    Add files via upload

--- lesson_18/js/script.js
+++ lesson_18/js/script.js
@@ -1,5 +1,11 @@
 
-const icon = document.querySelector('.icon-menu-burrer');
-icon.addEventListener('click', function () {
-	document.documentElement.classList.toggle('menu-open');
-});
+
+// const icon = document.querySelector('.icon-menu-burger');
+// icon.addEventListener('click', function () {
+// 	document.documentElement.classList.toggle('menu-open');
+// });
+
+// const icon = document.querySelector('.menu__body --jewelry');
+// icon.addEventListener('click', function () {
+// 	document.documentElement.classList.toggle('menu-open');
+// });
